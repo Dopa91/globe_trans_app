3cd527b679f431941a8be806ce12f6a635093946
dennis <info.dennis.durmus@gmail.com>
2024-10-24 13:53:11 +0200

    Bottom Navigator eingefügt ind für die Icons um auf dem entrechende seite zu gelangen funktion mit switch Case erstellt

--- lib/contactoverview/contact_overview.dart
+++ lib/contactoverview/contact_overview.dart
@@ -1,9 +1,32 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:globe_trans_app/chatOverView/chat_overview_screen.dart';
 import 'package:globe_trans_app/config/colors.dart';
 
-class ContactView extends StatelessWidget {
+class ContactView extends StatefulWidget {
   const ContactView({super.key});
+
+  @override
+  _ContactViewState createState() => _ContactViewState();
+}
+
+class _ContactViewState extends State<ContactView> {
+  int selectedPage = 0;
+  final List<Widget> _pageOptions = [
+    const ContactView(), // Kontakte Seite
+    const ChatView(), // Chat Seite
+    const Placeholder(), // Platzhalter für andere Seite
+  ];
+  String getAppBarTitle() {
+    switch (selectedPage) {
+      case 0:
+        return "Meine Kontakte";
+      case 1:
+        return "Chat Übersicht";
+      default:
+        return "Home";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +34,8 @@ class ContactView extends StatelessWidget {
       backgroundColor: backgroundColor,
       appBar: AppBar(
         centerTitle: true,
-        title: const Text(
-          "Meine Kontakte",
+        title: Text(
+          getAppBarTitle(),
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.black,
@@ -20,13 +43,14 @@ class ContactView extends StatelessWidget {
           ),
         ),
         backgroundColor: backgroundColor,
-        scrolledUnderElevation:
-            0, // Damit AppBar farbe sich nicht leicht verändert um den BackgroundColor identisch zu behalten
+        scrolledUnderElevation: 0,
         actions: <Widget>[
           GestureDetector(
             onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ChatView()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ChatView()),
+              );
             },
             child: const Padding(
               padding: EdgeInsets.only(right: 20),
@@ -44,84 +68,102 @@ class ContactView extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(35.0),
-            child: TextField(
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 20,
-                ),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide.none),
-                hintText: "Kontakte Suchen",
-                hintStyle: const TextStyle(
-                    fontSize: 16,
-                    fontFamily: "SFProDisplay",
-                    fontWeight: FontWeight.w400,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.grey),
-                prefixIcon:
-                    const Icon(Icons.search, size: 28, color: Colors.grey),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.clear, color: Colors.grey),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 100,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 3.0, horizontal: 35.0),
-                  child: Container(
-                    margin: EdgeInsets.only(right: 20),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(179, 255, 255, 255),
-                      border: Border.all(color: Colors.green),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black87.withOpacity(0.05),
-                          spreadRadius: 2,
-                          blurRadius: 6,
-                        ),
-                      ],
-                    ),
-                    child: const ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('assets/logo.png'),
+      body: selectedPage == 0
+          ? Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(35.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
                       ),
-                      title: Text(
-                        "Vorname Nachname",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                            color: Colors.black),
-                      ),
-                      subtitle: Text(
-                        "Online",
-                        style: TextStyle(
-                          color: Colors.green,
-                          fontSize: 14,
-                        ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide.none),
+                      hintText: "Kontakte Suchen",
+                      hintStyle: const TextStyle(
+                          fontSize: 16,
+                          fontFamily: "SFProDisplay",
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.grey),
+                      prefixIcon: const Icon(Icons.search,
+                          size: 28, color: Colors.grey),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.clear, color: Colors.grey),
+                        onPressed: () {},
                       ),
                     ),
                   ),
-                );
-              },
-            ),
-          ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 3.0, horizontal: 35.0),
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(179, 255, 255, 255),
+                            border: Border.all(color: Colors.green),
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black87.withOpacity(0.05),
+                                spreadRadius: 2,
+                                blurRadius: 6,
+                              ),
+                            ],
+                          ),
+                          child: const ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('assets/logo.png'),
+                            ),
+                            title: Text(
+                              "Vorname Nachname",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                  color: Colors.black),
+                            ),
+                            subtitle: Text(
+                              "Online",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            )
+          : _pageOptions[selectedPage],
+      bottomNavigationBar: ConvexAppBar(
+        backgroundColor: buttonColor,
+        color: Colors.white,
+        activeColor: Colors.white,
+        items: const [
+          TabItem(icon: Icons.contact_page, title: 'Kontakte'),
+          TabItem(icon: Icons.chat, title: 'Chats'),
+          TabItem(icon: Icons.settings, title: 'Einstellungen'),
         ],
+        initialActiveIndex: selectedPage,
+        onTap: (int index) {
+          setState(() {
+            selectedPage = index; // Wechselt die Seite bei Tab-Navigation
+          });
+        },
       ),
     );
   }
