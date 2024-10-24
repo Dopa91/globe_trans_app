a520a3696e1b28bbcd8d006cb10b6f6cb85198ae
dennis <info.dennis.durmus@gmail.com>
2024-10-24 22:01:43 +0200

    Story Board angelegt nur um zu gucken

--- lib/chatOverView/chat_overview_screen.dart
+++ lib/chatOverView/chat_overview_screen.dart
@@ -8,57 +8,93 @@ class ChatView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: backgroundColor,
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 3.0, horizontal: 35.0),
-            child: Container(
-              margin: const EdgeInsets.only(right: 20),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(179, 255, 255, 255),
-                border: Border.all(color: Colors.green),
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black87.withOpacity(0.05),
-                    spreadRadius: 2,
-                    blurRadius: 6,
-                  ),
-                ],
-              ),
-              child: ListTile(
-                leading: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: const DecorationImage(
-                      image: AssetImage('assets/logo.png'),
-                      fit: BoxFit.cover,
+      child: Column(
+        children: [
+          const Divider(color: Colors.green),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              storyItem('assets/logo.png', 'Mein Status'),
+              storyItem('assets/logo.png', 'Alice'),
+              storyItem('assets/logo.png', 'Bob'),
+              storyItem('assets/logo.png', 'John'),
+              storyItem('assets/logo.png', 'John'),
+              storyItem('assets/logo.png', 'John'),
+            ],
+          ),
+          const Divider(color: Colors.green),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 3.0, horizontal: 35.0),
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(179, 255, 255, 255),
+                      border: Border.all(color: Colors.green),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black87.withOpacity(0.05),
+                          spreadRadius: 2,
+                          blurRadius: 6,
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      leading: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/logo.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      title: const Text(
+                        "Vorname Nachname",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                            color: Colors.black),
+                      ),
+                      subtitle: const Text(
+                        "Online",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 14,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                title: const Text(
-                  "Vorname Nachname",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12,
-                      color: Colors.black),
-                ),
-                subtitle: const Text(
-                  "Online",
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 14,
-                  ),
-                ),
-              ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget storyItem(String imagePath, String name) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundImage: AssetImage(imagePath),
+        ),
+        const SizedBox(height: 5),
+        Text(
+          name,
+          style: const TextStyle(color: Colors.black),
+        ),
+      ],
     );
   }
 }
