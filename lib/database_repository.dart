1709d56e629bb095e8ff84737b4452218c5ca5c2
dennis <info.dennis.durmus@gmail.com>
2024-11-14 17:12:17 +0100

    Database Repo

---
+++ lib/database_repository.dart
@@ -0,0 +1,28 @@
+import 'package:globe_trans_app/features/chat_feature/presentation/chat_screen.dart';
+
+abstract class DatabaseRepository {
+  // Message
+
+  // Sende Nachrichten
+  Future<void> sendMessage(Message message);
+
+  // Löschen einer Nachricht
+  Future<void> deleteMessage(Message message);
+
+  // Update einer Nachricht
+  Future<void> updateMessage(
+      Message message, String newContent, String newTimeStamp);
+
+  // Übersicht aller Nachrichten
+  Future<List<Message>> getAllMessages();
+
+  // Hier kommt das ganze für Chat hin
+
+  Future<void> newGroupChat(List<Message> messages);
+
+  // Neue chat erstellen
+  Future<void> createChat(Message message, String receiver);
+
+  // Übersicht aller chats
+  Future<List<Chat>> getAllChats();
+}
