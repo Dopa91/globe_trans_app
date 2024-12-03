182e7a60bfa49f263e8c39e2e347001d383a4db8
dennis <info.dennis.durmus@gmail.com>
2024-12-03 15:06:41 +0100

    Class Contact Angelegt

---
+++ lib/features/adcontact_feature/presentation/class.contact.dart
@@ -0,0 +1,18 @@
+class Contact {
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String? image;
+
+  Contact({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.image,
+  });
+
+  // @override
+  // String toString() {
+  //   return 'Contact{name: $name, email: $email, phoneNumber: $phoneNumber, image: $image}';
+  // }
+}
