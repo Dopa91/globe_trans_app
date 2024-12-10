7690189fa622b0711c14c038f7c9242bf13753a6
dennis <info.dennis.durmus@gmail.com>
2024-12-10 11:08:36 +0100

    added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:globe_trans_app/config/themes.dart';
 import 'package:globe_trans_app/features/register_feature/presentation/homescreen/home_screen.dart';
 import 'package:globe_trans_app/features/shared/database_repository.dart';
 import 'package:globe_trans_app/features/shared/mock_database.dart';
+import 'package:globe_trans_app/firebase_options.dart';
+
+// void main() async {
+//   final repository = MockDatabase();
+//   runApp(MyApp(repository: repository));
+// }
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final repository = MockDatabase();
   runApp(MyApp(repository: repository));
 }
