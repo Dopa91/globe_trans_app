15696f2edc4f680966a36bcf8197493c8796979c
dennis <info.dennis.durmus@gmail.com>
2024-12-20 17:05:44 +0100

    Firebase Database Repo has been revised

--- lib/features/shared/firebase_database_repository.dart
+++ lib/features/shared/firebase_database_repository.dart
@@ -3,22 +3,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:globe_trans_app/features/adcontact_feature/presentation/class.contact.dart';
 import 'package:globe_trans_app/features/chat_feature/presentation/chat_screen.dart';
 import 'package:globe_trans_app/features/shared/database_repository.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseDatabaseRepository implements DatabaseRepository {
+  // Kontakt hinzufügen
   @override
   Future<void> addContact(
       String name, String email, String phoneNumber, String image) async {
     final firestore = FirebaseFirestore.instance;
     String userId = await getUserId();
-    await firestore.collection("users").doc(userId).collection('contacts').add({
-      'name': name,
-      'email': email,
-      'phoneNumber': phoneNumber,
-      'image': image,
+    await firestore.collection("users").doc(userId).collection("contacts").add({
+      "name": name,
+      "email": email,
+      "phoneNumber": phoneNumber,
+      "image": image,
     });
   }
 
+  // Benutzer-ID abrufen
   Future<String> getUserId() async {
     try {
       FirebaseAuth auth = FirebaseAuth.instance;
@@ -26,18 +27,19 @@ class FirebaseDatabaseRepository implements DatabaseRepository {
       if (user != null) {
         return user.uid;
       } else {
-        print('Kein authentifizierter Benutzer gefunden.');
+        print("Kein authentifizierter Benutzer gefunden.");
         return '';
       }
     } catch (e) {
-      print('Fehler beim Abrufen der Benutzer-ID: $e');
+      print("Fehler beim Abrufen der Benutzer-ID: $e");
       return '';
     }
   }
 
-  // chat erstellen
   @override
-  Future<void> createChat(Message message, String receiver) async {}
+  Future<void> createChat(Message message, String receiver) async {
+    // Muss noch implementiert werden
+  }
 
   // Kontakt löschen
   @override
@@ -52,19 +54,20 @@ class FirebaseDatabaseRepository implements DatabaseRepository {
         .delete();
   }
 
-  // Nachricht löschen
   @override
-  Future<void> deleteMessage(Message message) async {}
+  Future<void> deleteMessage(Message message) async {
+    // Muss noch implementiert werden
+  }
 
   @override
-  Future<List<Chat>> getAllChats() {
-    // TODO: implement getAllChats
+  Future<List<Chat>> getAllChats() async {
+    // Muss noch implementiert werden
     throw UnimplementedError();
   }
 
   final List<Message> _messages = [];
 
-  // Liste aller Nachrichen
+  // Rufe alle Nachrichten ab
   @override
   Future<List<Message>> getAllMessages() async {
     await Future.delayed(const Duration(seconds: 1));
@@ -72,9 +75,11 @@ class FirebaseDatabaseRepository implements DatabaseRepository {
   }
 
   @override
-  Future<void> getContact(Contact contact) async {}
+  Future<void> getContact(Contact contact) async {
+    // Muss noch implementiert werden
+  }
 
-  // Kontakt Liste
+  // Rufe Kontaktliste ab
   @override
   Future<List<Contact>> getContactList() async {
     final firestore = FirebaseFirestore.instance;
@@ -93,59 +98,25 @@ class FirebaseDatabaseRepository implements DatabaseRepository {
         .toList();
   }
 
-  // GruppenChat Liste
   @override
-  Future<void> newGroupChat(List<Message> messages) async {}
-
-  // Kontakte Speichern
-  @override
-  Future<void> saveContactList(List<Contact> contacts) async {}
-
-  // Nachricht senden
-  @override
-  Future<void> sendMessage(Message message) async {}
-
-  // Verification Code
+  Future<void> newGroupChat(List<Message> messages) async {
+    // Muss noch implementiert werden
+  }
 
   @override
-  Future<void> sendVerificationCode(String phoneNumber) async {
-    await FirebaseAuth.instance.verifyPhoneNumber(
-      phoneNumber: phoneNumber,
-      verificationCompleted: (PhoneAuthCredential credential) {
-        FirebaseAuth.instance.signInWithCredential(credential);
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        throw Exception("Fehler bei der Verifizierung: ${e.message}");
-      },
-      codeSent: (String verificationId, int? resendToken) {
-        print("Code gesendet: $verificationId");
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {
-        print("Auto-Retrieval Timeout: $verificationId");
-      },
-    );
+  Future<void> saveContactList(List<Contact> contacts) async {
+    // Muss noch implementiert werden
+  }
+
+  @override
+  Future<void> sendMessage(Message message) async {
+    // Muss noch implementiert werden
   }
 
   @override
   Future<void> updateMessage(
-      Message message, String newContent, String newTimeStamp) {
-    // TODO: implement updateMessage
+      Message message, String newContent, String newTimeStamp) async {
+    // Muss noch implementiert werden
     throw UnimplementedError();
-  }
-
-  @override
-  Future<void> signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-      final GoogleSignInAuthentication? googleAuth =
-          await googleUser?.authentication;
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth?.accessToken,
-        idToken: googleAuth?.idToken,
-      );
-      await FirebaseAuth.instance.signInWithCredential(credential);
-    } on Exception catch (e) {
-      print('exception->$e');
-    }
   }
 }
