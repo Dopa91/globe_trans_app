8c1db48cb1e1f84af0b587cd3adefd363aaaa491
dennis <info.dennis.durmus@gmail.com>
2024-12-20 17:16:37 +0100

    ChatView Screen has been edited

--- lib/features/chat_overview_feature/presentation/chat_overview_screen.dart
+++ lib/features/chat_overview_feature/presentation/chat_overview_screen.dart
@@ -15,88 +15,93 @@ class ChatView extends StatefulWidget {
 class _ChatViewState extends State<ChatView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: backgroundColor,
-      child: Column(
-        children: [
-          const Divider(color: Colors.green),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Scaffold(
+      body: Container(
+        color: backgroundColor,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              storyItem("assets/statusfoto.jpg", "Mein Status"),
-              storyItem("assets/mertkontaktfoto.jpeg", "Mert "),
-              storyItem("assets/melekkontaktfoto.jpeg", "Melek "),
-              storyItem("assets/direnckontaktfoto.jpeg", "Direnc"),
-              storyItem('assets/logo.png', 'Iso'),
-              storyItem("assets/yelizkontaktfoto.jpeg", "Yeliz"),
+              const Divider(color: Colors.green),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  storyItem("assets/statusfoto.jpg", "Mein Status"),
+                  storyItem("assets/mertkontaktfoto.jpeg", "Mert "),
+                  storyItem("assets/melekkontaktfoto.jpeg", "Melek "),
+                  storyItem("assets/direnckontaktfoto.jpeg", "Direnc"),
+                  storyItem('assets/logo.png', 'Iso'),
+                  storyItem("assets/yelizkontaktfoto.jpeg", "Yeliz"),
+                ],
+              ),
+              const Divider(color: Colors.green),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: names.length,
+                  itemBuilder: (context, index) {
+                    String contactName = names[index]; //
+
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 3.0, horizontal: 35.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(179, 255, 255, 255),
+                          border: Border.all(color: Colors.green),
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black87.withOpacity(0.05),
+                              spreadRadius: 2,
+                              blurRadius: 6,
+                            ),
+                          ],
+                        ),
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation, secondaryAnimation) =>
+                                        ChatScreen(
+                                  contactName: contactName,
+                                ),
+                                transitionsBuilder: (context, animation,
+                                    secondaryAnimation, child) {
+                                  const begin = Offset(1.0, 0.0);
+                                  const end = Offset.zero;
+                                  const curve = Curves.easeInOut;
+
+                                  var tween = Tween(begin: begin, end: end)
+                                      .chain(CurveTween(curve: curve));
+                                  var offsetAnimation = animation.drive(tween);
+
+                                  return SlideTransition(
+                                    position: offsetAnimation,
+                                    child: child,
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                          leading: const CircleAvatar(
+                            backgroundImage: AssetImage('assets/logo.png'),
+                          ),
+                          title: Text(contactName,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          subtitle: const Text("Letzte Nachricht",
+                              style: TextStyle(color: Colors.grey)),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
-          const Divider(color: Colors.green),
-          Expanded(
-            child: ListView.builder(
-              itemCount: names.length,
-              itemBuilder: (context, index) {
-                String contactName = names[index]; //
-
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 3.0, horizontal: 35.0),
-                  child: Container(
-                    margin: const EdgeInsets.only(right: 20),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(179, 255, 255, 255),
-                      border: Border.all(color: Colors.green),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black87.withOpacity(0.05),
-                          spreadRadius: 2,
-                          blurRadius: 6,
-                        ),
-                      ],
-                    ),
-                    child: ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          PageRouteBuilder(
-                            pageBuilder:
-                                (context, animation, secondaryAnimation) =>
-                                    ChatScreen(
-                              contactName: contactName,
-                            ),
-                            transitionsBuilder: (context, animation,
-                                secondaryAnimation, child) {
-                              const begin = Offset(1.0, 0.0);
-                              const end = Offset.zero;
-                              const curve = Curves.easeInOut;
-
-                              var tween = Tween(begin: begin, end: end)
-                                  .chain(CurveTween(curve: curve));
-                              var offsetAnimation = animation.drive(tween);
-
-                              return SlideTransition(
-                                position: offsetAnimation,
-                                child: child,
-                              );
-                            },
-                          ),
-                        );
-                      },
-                      leading: const CircleAvatar(
-                        backgroundImage: AssetImage('assets/logo.png'),
-                      ),
-                      title: Text(contactName,
-                          style: const TextStyle(fontWeight: FontWeight.bold)),
-                      subtitle: const Text("Letzte Nachricht",
-                          style: TextStyle(color: Colors.grey)),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
