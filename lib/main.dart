65e85c5aa18369f30eedc72543be6b00e0c20a09
dennis <info.dennis.durmus@gmail.com>
2024-12-20 17:19:05 +0100

    main.dart was edited and Streambuilder was added

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:globe_trans_app/config/themes.dart';
-import 'package:globe_trans_app/features/register_feature/presentation/homescreen/home_screen.dart';
+import 'package:globe_trans_app/features/contact_overview_feature/presentation/contact_overview.dart';
+import 'package:globe_trans_app/features/login_screen/presentation/login_home_screen.dart';
+import 'package:globe_trans_app/features/shared/auth_repository.dart';
 import 'package:globe_trans_app/features/shared/database_repository.dart';
+import 'package:globe_trans_app/features/shared/firebase_auth_repository.dart';
 import 'package:globe_trans_app/features/shared/firebase_database_repository.dart';
 import 'package:globe_trans_app/firebase_options.dart';
 import 'package:provider/provider.dart';
-
-// void main() async {
-//   final repository = MockDatabase();
-//   runApp(MyApp(repository: repository));
-// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +21,10 @@ void main() async {
   runApp(MultiProvider(providers: [
     Provider<DatabaseRepository>(
       create: (_) => FirebaseDatabaseRepository(),
-    )
+    ),
+    Provider<AuthRepository>(
+      create: (_) => FirebaseAuthRepository(),
+    ),
   ], child: MyApp()));
 }
 
@@ -38,10 +39,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'GlobeTransMessage',
-      theme: myTheme,
-      home: const HomeScreen(),
-    );
+        theme: myTheme,
+        debugShowCheckedModeBanner: false,
+        title: 'GlobeTransMessage',
+        home: Scaffold(
+            body: StreamBuilder<User?>(
+                stream: authInstance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    User? user = snapshot.data;
+                    print("User Logged In: ${user != null}");
+                    if (user == null) {
+                      return const LoginScreen(); // Login Screen
+                    } else {
+                      return const ContactView(
+                        startPage: 1,
+                      ); // Angemeldete User starten direkt in ChatOverView Screen
+                    }
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                })));
   }
 }
+
+                    
+     
+                   
+        
+          
+        
+      
+      // initialRoute: "/",
+      // routes: {
+      //   "/": (context) => const SplashScreen(),
+      //   "/home": (context) => const HomeScreen()
+      // },
+    
+  
+
