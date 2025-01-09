39b15e7453a0cb33e5fe670ad1185fbeb8421e6a
dennis <info.dennis.durmus@gmail.com>
2025-01-09 09:36:04 +0100

    After logging out, the login screen is displayed

--- lib/features/setting/presentation/settings_screen.dart
+++ lib/features/setting/presentation/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:globe_trans_app/features/login_screen/presentation/login_home_screen.dart';
 import 'package:globe_trans_app/features/setting/widget/change_mobile_number.dart';
 import 'package:globe_trans_app/features/setting/widget/create_profile.dart';
 import 'package:globe_trans_app/features/shared/auth_repository.dart';
@@ -90,7 +91,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
             title:
                 const Text("Ausloggen", style: TextStyle(color: Colors.white)),
             trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white),
-            onTap: () => authRepository.signOutFromGoogle(),
+            onTap: () {
+              authRepository.signOutFromGoogle();
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
           ),
         ],
       ),
