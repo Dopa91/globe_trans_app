663fb87d2b9c8b4ace220955a237ca33d3c82e6f
dennis <info.dennis.durmus@gmail.com>
2025-01-10 09:35:44 +0100

    Hard coded contacts have been removed, the contacts are taken from the contact list

--- lib/features/chat_overview_feature/presentation/chat_overview_screen.dart
+++ lib/features/chat_overview_feature/presentation/chat_overview_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:globe_trans_app/config/colors.dart';
+import 'package:globe_trans_app/features/adcontact_feature/presentation/class.contact.dart';
 import 'package:globe_trans_app/features/chat_feature/presentation/chat_screen.dart';
-import 'package:globe_trans_app/features/shared/name_repo.dart';
+import 'package:globe_trans_app/features/shared/database_repository.dart';
+import 'package:provider/provider.dart';
 
 class ChatView extends StatefulWidget {
   const ChatView({
@@ -37,64 +39,88 @@ class _ChatViewState extends State<ChatView> {
               ),
               const Divider(color: Colors.green),
               Expanded(
-                child: ListView.builder(
-                  itemCount: names.length,
-                  itemBuilder: (context, index) {
-                    String contactName = names[index]; //
+                child: FutureBuilder<List<Contact>>(
+                  future: context.read<DatabaseRepository>().getContactList(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
 
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 3.0, horizontal: 35.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(179, 255, 255, 255),
-                          border: Border.all(color: Colors.green),
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black87.withOpacity(0.05),
-                              spreadRadius: 2,
-                              blurRadius: 6,
-                            ),
-                          ],
-                        ),
-                        child: ListTile(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              PageRouteBuilder(
-                                pageBuilder:
-                                    (context, animation, secondaryAnimation) =>
-                                        ChatScreen(
-                                  contactName: contactName,
+                    if (snapshot.hasError) {
+                      return const Center(
+                          child: Text("Fehler beim Laden der Kontakte"));
+                    }
+
+                    if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(
+                          child: Text("Keine Kontakte vorhanden"));
+                    }
+
+                    final contacts = snapshot.data!;
+
+                    return ListView.builder(
+                      itemCount: contacts.length,
+                      itemBuilder: (context, index) {
+                        final contact = contacts[index];
+                        final contactName =
+                            "${contact.firstName} ${contact.lastName}";
+
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 3.0, horizontal: 35.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(179, 255, 255, 255),
+                              border: Border.all(color: Colors.green),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black87.withOpacity(0.05),
+                                  spreadRadius: 2,
+                                  blurRadius: 6,
                                 ),
-                                transitionsBuilder: (context, animation,
-                                    secondaryAnimation, child) {
-                                  const begin = Offset(1.0, 0.0);
-                                  const end = Offset.zero;
-                                  const curve = Curves.easeInOut;
+                              ],
+                            ),
+                            child: ListTile(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  PageRouteBuilder(
+                                    pageBuilder: (context, animation,
+                                            secondaryAnimation) =>
+                                        ChatScreen(
+                                      contactName: contactName,
+                                    ),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      const begin = Offset(1.0, 0.0);
+                                      const end = Offset.zero;
+                                      const curve = Curves.easeInOut;
 
-                                  var tween = Tween(begin: begin, end: end)
-                                      .chain(CurveTween(curve: curve));
-                                  var offsetAnimation = animation.drive(tween);
+                                      var tween = Tween(begin: begin, end: end)
+                                          .chain(CurveTween(curve: curve));
+                                      var offsetAnimation =
+                                          animation.drive(tween);
 
-                                  return SlideTransition(
-                                    position: offsetAnimation,
-                                    child: child,
-                                  );
-                                },
+                                      return SlideTransition(
+                                        position: offsetAnimation,
+                                        child: child,
+                                      );
+                                    },
+                                  ),
+                                );
+                              },
+                              leading: const CircleAvatar(
+                                backgroundImage: AssetImage('assets/logo.png'),
                               ),
-                            );
-                          },
-                          leading: const CircleAvatar(
-                            backgroundImage: AssetImage('assets/logo.png'),
+                              title: Text(contactName,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold)),
+                              subtitle: const Text("Letzte Nachricht",
+                                  style: TextStyle(color: Colors.grey)),
+                            ),
                           ),
-                          title: Text(contactName,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold)),
-                          subtitle: const Text("Letzte Nachricht",
-                              style: TextStyle(color: Colors.grey)),
-                        ),
-                      ),
+                        );
+                      },
                     );
                   },
                 ),
